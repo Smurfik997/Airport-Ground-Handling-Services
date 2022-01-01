494d58b206e8345f8a5c3c89ac22716b90704279
Artem Tryhub <tryhub.artem@quotie.app>
2022-01-01 22:32:57 +0200

    UPD: init configs for MySQL database

---
+++ initdb.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA `service-users` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE SCHEMA `service-aircrafts` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE SCHEMA `service-requests` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE SCHEMA `service-parking` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE SCHEMA `service-billings` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
